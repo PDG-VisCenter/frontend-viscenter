99d6e25d2762de4d812b80f54d202bf79999abeb
Gabriela Gonzales Villarroel <gonzales.villarroel.gabriela@gmail.com>
2024-09-13 12:19:02 -0400

    Fix eslint problems

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,5 @@
 import { encrypt } from '../../../../utils/encryption';
 import { jwtDecode } from 'jwt-decode';
-
 import KeycloakProvider from 'next-auth/providers/keycloak';
 import NextAuth from 'next-auth';
 
@@ -41,30 +40,33 @@ export const authOptions = {
     async jwt({ token, account }) {
       const nowTimeStamp = Math.floor(Date.now() / 1000);
 
+      // account is only available the first time this callback is called on a new session (after the user signs in)
       if (account) {
-        // account is only available the first time this callback is called on a new session (after the user signs in)
         token.decoded = jwtDecode(account.access_token);
         token.access_token = account.access_token;
         token.id_token = account.id_token;
         token.expires_at = account.expires_at;
         token.refresh_token = account.refresh_token;
         return token;
-      } else if (nowTimeStamp < token.expires_at) {
-        // token has not expired yet, return it
+      }
+
+      // token has not expired yet, return it
+      if (nowTimeStamp < token.expires_at) {
         return token;
-      } else {
-        // token is expired, try to refresh it
-        console.log('Token has expired. Will refresh...');
-        try {
-          const refreshedToken = await refreshAccessToken(token);
-          console.log('Token is refreshed.');
-          return refreshedToken;
-        } catch (error) {
-          console.error('Error refreshing access token', error);
-          return { ...token, error: 'RefreshAccessTokenError' };
-        }
+      }
+
+      // token is expired, refresh it
+      try {
+        const refreshedToken = await refreshAccessToken(token);
+        return refreshedToken;
+      } catch (error) {
+        return {
+          ...token,
+          error: 'RefreshAccessTokenError',
+        };
       }
     },
+
     async session({ session, token }) {
       // Send properties to the client
       session.access_token = encrypt(token.access_token);
