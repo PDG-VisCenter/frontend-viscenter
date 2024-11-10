13b5fc0b6c6f501a67aca1ddebfa0bcbff5cc746
Gabriela Gonzales Villarroel <gonzales.villarroel.gabriela@gmail.com>
2024-11-10 14:49:05 -0400

    Connect states with Product review step

--- src/app/admin/addproduct/ProductReview.jsx
+++ src/app/admin/addproduct/ProductReview.jsx
@@ -1,58 +1,78 @@
+import { brands, categoriesAndSubcategories, colors } from '@/data/searchFilters';
 import { Descriptions } from 'antd';
 import Title from 'antd/es/typography/Title';
+import { useSelector } from 'react-redux';
 
-const productDetailsItems = [
-  {
-    label: 'Nombre',
-    children: 'Cloud Database',
-  },
-  {
-    label: 'Precio',
-    children: 'Prepaid',
-  },
-  {
-    label: 'Marca',
-    children: '18:00:00',
-  },
-  {
-    label: 'Categoria',
-    children: '$80.00',
-  },
-  {
-    label: 'Forma',
-    children: '$20.00',
-  },
-  {
-    label: 'Material',
-    children: '$60.00',
-  },
-  {
-    label: 'Colores',
-    children: (
-      <>
-        Data disk type: MongoDB
-        <br />
-        Database version: 3.4
-        <br />
-        Package: dds.mongo.mid
-      </>
-    ),
-  },
-  {
-    label: 'Descripción',
-    children: (
-      <>
-        Data disk type: MongoDB
-        <br />
-        Database version: 3.4
-        <br />
-        Package: dds.mongo.mid
-      </>
-    ),
-  },
-];
+function getCategoryAndSubcategoryLabel(categoryArray) {
+  if (categoryArray == null) return '';
+
+  const category = categoriesAndSubcategories.find((cat) => cat.value === categoryArray[0]);
+  const subcategory = category.children.find((sub) => sub.value === categoryArray[1]);
+
+  return `${category.label} / ${subcategory.label}`;
+}
+
+function getBrandLabel(brandId) {
+  if (brandId == null) return '';
+  const brand = brands.find((brand) => brand.value === brandId);
+  return brand.label;
+}
+
+function getColorLabelByValue(colorId) {
+  const color = colors.find((c) => c.value === colorId);
+  return color ? color.label : '';
+}
 
 function ProductReview() {
+  const productData = useSelector((state) => state.addProduct);
+  const productItemsData = useSelector((state) => state.addProductItem.addProductItems);
+  console.log(productItemsData);
+
+  const productDetailsItems = [
+    {
+      label: 'Nombre',
+      children: `${productData.name || ''}`,
+    },
+    {
+      label: 'Precio',
+      children: `Bs. ${productData.price || '0'}`,
+    },
+    {
+      label: 'Marca',
+      children: getBrandLabel(productData.brand),
+    },
+    {
+      label: 'Categoria',
+      children: getCategoryAndSubcategoryLabel(productData.category),
+    },
+    {
+      label: 'Forma',
+      children: `${productData.shape || ''}`,
+    },
+    {
+      label: 'Material',
+      children: `${productData.material || ''}`,
+    },
+    {
+      label: 'Colores/Items',
+      children: (
+        <>
+          {productItemsData.map((item) => (
+            <div key={item.id}>{getColorLabelByValue(item.color)}</div>
+          ))}
+          <br />
+          Database version: 3.4
+          <br />
+          Package: dds.mongo.mid
+        </>
+      ),
+    },
+    {
+      label: 'Descripción',
+      children: `${productData.description || ''}`,
+    },
+  ];
+
   return (
     <div>
       <Title
